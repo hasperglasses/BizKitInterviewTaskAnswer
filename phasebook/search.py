08847b03e3b1ca4aadc08db33f6b4eb2e8b38f40
hasperglasses <hasperglasses@gmail.com>
2023-04-21 23:02:00 +0800

    Test answers

--- phasebook/search.py
+++ phasebook/search.py
@@ -10,6 +10,18 @@ bp = Blueprint("search", __name__, url_prefix="/search")
 def search():
     return search_users(request.args.to_dict()), 200
 
+def search_id(arg):
+    match arg[0]:
+        case "id":
+            return [d["id"] for d in USERS if d.get(arg[0]) == arg[1]]
+        case "name":
+            return [d["id"] for d in USERS if arg[1].lower() in d.get(arg[0]).lower()]
+        case "age":
+            return [d["id"] for d in USERS if int(d.get(arg[0])) in range(int(arg[1])-1,int(arg[1])+2)]
+        case "occupation":
+            return [d["id"] for d in USERS if arg[1].lower() in d.get(arg[0]).lower()]
+    return None
+    
 
 def search_users(args):
     """Search users database
@@ -26,5 +38,9 @@ def search_users(args):
     """
 
     # Implement search here!
+    id_list = []
+    for arg in args.items():
+        id_list += search_id(arg)
 
-    return USERS
+
+    return [d for d in USERS if d["id"] in id_list]
